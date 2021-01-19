a1142fc432a710a19842a3fb155bf5342b1660cd
Aschwin Wester <aschwin.wester@gmail.com>
2021-01-19 20:12:13 +0100

    create server to stop later

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"os"
+	"fmt"
 	"github.com/aschwinwester/ingestion-scheduling/rest"
 )	
 
+func isStartServer(args []string) bool {
+	start := false
+	server := false
+	for i := 0; i < len(args); i++ {
+		if args[i] == "start" {
+			start = true
+		}
+		if args[i] == "server" {
+			server = true
+		}
+	}
+	return start && server
+}
 func main() {
-
-	rest.StartServer()
+	args := os.Args
+	if isStartServer(args) {
+		rest.StartServer()
+	} else {
+		fmt.Println("Nothing to do")
+	}
+	fmt.Println("Done!")
 }
--- rest/router.go
+++ rest/router.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"net/http"
 	"fmt"
+	"time"
 	"github.com/gorilla/mux"
 )
 
@@ -13,10 +14,22 @@ func CreateRouter() *mux.Router {
 		treatment := vars["treatment"]
 		fmt.Fprintf(w, "Treatment %s \n", treatment)
 	})
+	r.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		treatment := vars["treatment"]
+		fmt.Fprintf(w, "Treatment %s \n", treatment)
+	})
 	return r
 	
 }
 
 func StartServer() {
-	http.ListenAndServe(":8080", CreateRouter())
+	srv := &http.Server{
+		Handler:      CreateRouter(),
+		Addr:         "127.0.0.1:8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	srv.ListenAndServe()
 }
